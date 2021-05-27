9c27b3f6c6a45240e189b9003285545e6082efe4
Ramneet Kaur Bhutani <ramneet.bhutani13@gmail.com>
2021-05-27 23:15:25 +0530

    Changes done and added Implicit conversion

--- TypeCasting.java
+++ TypeCasting.java
@@ -14,7 +14,27 @@ public class TypeCasting {
 	}
 	
 	public static void main(String[] args) {
-	
+//Implicit Type Conversion		
+		System.out.println("Implicit Type conversion");
+		
+		char c = 'A';
+		System.out.println("Value of character: " +c);
+		
+		int integer1 = c;
+		System.out.println("character to integer: " +integer1);
+		
+		long integer2 = c;
+		System.out.println("character to long: " +integer2);
+		
+		float decimal1 = c;
+		System.out.println("character to float: " +decimal1);
+		
+		double decimal2 = c;
+		System.out.println("character to double: " +decimal2);
+			
+// Explicit Type Conversion
+		System.out.println("Explicit Type conversion");
+		
 			TypeCasting typecasting = new TypeCasting("8765846789");
 		
 			double d = Double.parseDouble(typecasting.getStr());
@@ -23,8 +43,9 @@ public class TypeCasting {
 		float f = (float) d;
 		short s = (short) d;
 		long l = (long) d;
-		System.out.println("integer: " + i + "\nbyte: " + b + "\nfloat: " + f + "\nshort: " + s +
-				"\nlong: " + l + "\ndouble: " + d);
+		System.out.println("Value of double: " +d );
+		System.out.println("Double to integer: " + i + "\nDouble to byte: " + b 
+		+ "\nDouble to float: " + f + "\nDouble to short: " + s + "\nDouble to long: " + l );
 		
 		
 		
