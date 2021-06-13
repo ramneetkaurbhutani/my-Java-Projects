24f736e7fcd491350a3aa94d1b7ce1ba78bda106
Ramneet Kaur Bhutani <ramneet.bhutani13@gmail.com>
2021-06-13 16:29:39 +0530

    Java Project Phase-1 Operations to be performed in the project

---
+++ Operations.java
@@ -0,0 +1,13 @@
+
+
+public interface Operations {
+	
+public boolean fileNameValidity(String fileName);
+public void displayFiles();
+public void addFile(String fileName);
+public void deleteFile(String fileName);
+public void searchFile(String fileName);
+public void mainMenu();
+public void subMenu();
+
+}
